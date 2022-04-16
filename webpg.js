d54033585055385663be886dd0ae10a06dc9a302
christopheralphonse <christopheralphonse96@gmail.com>
2022-04-15 23:22:17 -0400

    first commit

---
+++ webpg.js
@@ -0,0 +1,8 @@
+const bgChanger = () => {
+  if (this.scrollY > this.innerHeight / 1.2) {
+    document.body.classList.add("bg-active");
+  } else {
+    document.body.classList.remove("bg-active");
+  }
+};
+window.addEventListener("scroll", bgChanger);
